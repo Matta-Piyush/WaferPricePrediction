600fcd84db175d4452ef15d9bade955c7342dd33
Matta-Piyush <piyushmatta26@gmail.com>
2023-07-18 22:27:20 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='WaferPricePrediction',
+    version='0.0.1',
+    author='PIYUSH MATTA',
+    author_email='piyushmatta26@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
